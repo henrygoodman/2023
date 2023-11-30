9ee59f6f0fdf4947bf774165133c97f30d945954
Henry Goodman <13032204@student.uts.edu.au>
2023-11-30 19:23:08 +1100

    Add project template

---
+++ src/day1/mod.rs
@@ -0,0 +1,13 @@
+pub fn solve1(input: Vec<String>) -> i32 {
+    for line in input {
+        println!("{:?}", line);
+    }
+    5
+}
+
+pub fn solve2(input: Vec<String>) -> i32 {
+    for line in input {
+        println!("{:?}", line);
+    }
+    5
+}
---
+++ src/main.rs
@@ -0,0 +1,43 @@
+const CURRENT_DAY: i32 = 1;  // Set the current day here
+
+mod day1;
+
+use std::env;
+use std::path::PathBuf;
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn read_lines(filename: PathBuf) -> io::Result<Vec<String>> {
+    let file = File::open(filename)?;
+    let buf_reader = io::BufReader::new(file);
+    buf_reader.lines().collect()
+}
+
+fn main() -> io::Result<()> {
+    // Get the directory containing Cargo.toml
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    let input1_path = PathBuf::from(manifest_dir.clone())
+        .join("src")
+        .join("inputs")
+        .join(format!("day{}", CURRENT_DAY))
+        .join("1.txt");
+    
+    let input2_path = PathBuf::from(manifest_dir.clone())
+        .join("src")
+        .join("inputs")
+        .join(format!("day{}", CURRENT_DAY))
+        .join("2.txt");
+
+    match CURRENT_DAY {
+        1 => {
+            let input1 = read_lines(input1_path)?;
+            let input2 = read_lines(input2_path)?;
+            println!("{:?}", day1::solve1(input1));
+            println!("{:?}", day1::solve2(input2));
+        },
+        _ => println!("Day not implemented"),
+    }
+
+    Ok(())
+}
